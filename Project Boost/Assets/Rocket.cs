1ddd8a63fd27700bda9b2ee2c634495e1edd0424
Rushal <TheRushal@gmail.com>
2018-07-21 23:34:08 -0500

    Lesson 52

--- Project Boost/Assets/Rocket.cs
+++ Project Boost/Assets/Rocket.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    [SerializeField] float rcsThrust = 250f;
+    [SerializeField] float mainThrust = 500f;
+
     private Rigidbody rigidbody;
     private AudioSource audioSource;
 
@@ -16,32 +19,58 @@ public class Rocket : MonoBehaviour {
 	
 	void Update ()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            case "Fuel":
+                print("FUEL");
+                break;
+            default:
+                print("DEAD");
+                break;
+
+        }
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidbody.AddRelativeForce(Vector3.up);
+            rigidbody.AddRelativeForce(Vector3.up * mainThrust);
 
-            if(!audioSource.isPlaying)
+            if (!audioSource.isPlaying)
             {
                 audioSource.Play();
             }
-            else
-            {
-                audioSource.Stop();
-            }
         }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true; // Take manual control of rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+
+        rigidbody.freezeRotation = false;
     }
 }
